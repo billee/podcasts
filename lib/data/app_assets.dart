c814854ea315f833c6581b60e429642e22df1e6d
billee <twilly.t@gmail.com>
2025-07-13 12:17:06 -0400

    fix the typo error on app_assets.dart

--- lib/data/app_assets.dart
+++ lib/data/app_assets.dart
@@ -4,15 +4,15 @@ class AppAssets {
   static const List<String> podcastAssets = [
     // Renamed to podcastAssets
     'assets/podcast_sources/ang_kulturang_filipino_sa_saudi_arabia.wav',
-    'assets/podcast_sources/buhay_hamon_at_pag-angkop.wav',
+    'assets/podcast_sources/buhay_hamon_at_pag_angkop.wav',
     'assets/podcast_sources/higit_pa_sa_padala.wav',
     'assets/podcast_sources/kainipan_at_pangungulila.wav',
     'assets/podcast_sources/kalusugan_ng_kababaihang_OFW.wav',
-    'assets/podcast_sources/Kultural_na_Kapistahan_Abu_Dhabi.wav',
-    'assets/podcast_sources/mag-isa_sa_ibang_bansa.wav',
+    'assets/podcast_sources/kultural_na_kapistahan_abu_dhabi.wav',
+    'assets/podcast_sources/mag_isa_sa_ibang_bansa.wav',
     'assets/podcast_sources/mahalagang_kalusugan_ng_isip.wav',
-    'assets/podcast_sources/mga_pista_ng _pilipinas.wav',
-    'assets/podcast_sources/Pilipinong_nasa_30s_sa_ibang_bansa.wav',
+    'assets/podcast_sources/mga_pista_ng_pilipinas.wav',
+    'assets/podcast_sources/pilipinong_nasa_30s_sa_ibang_bansa.wav',
     'assets/podcast_sources/regalo_at_tradisyon_sa_pilipinas.wav',
     'assets/podcast_sources/oportunidad_para_sa_mga_OFW.wav',
   ];
